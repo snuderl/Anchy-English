22bd558b6ce1f93e850408ab3bdd389c7e49d2ce
BLaž Šnuderl <snuderl@gmail.com>
2013-11-09 23:06:36 +0100

    Config updated

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__, static_folder='app', static_url_path='')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL'] #'sqlite:///baza.sqlite'
+db_url = os.environ.get('DATABASE_URL')
+if not db_url:
+	db_url = 'sqlite:///baza.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = db_url
 db = SQLAlchemy(app)
